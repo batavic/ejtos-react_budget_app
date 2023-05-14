dadf6dedaa29c443e6a8402d84911029611f4189
Victoria Bata <85346019+batavic@users.noreply.github.com>
2023-05-14 19:12:57 +0300

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,68 @@
 
-
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
+
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+ const { budget, expenses, dispatch } = useContext(AppContext);
+ const [updatedBudget, setUpdatedBudget] = useState(budget);
+
+
+ useEffect(() => {
+   setUpdatedBudget(budget);
+ }, [budget]);
+
+
+ useEffect(() => {
+   const totalExpenses = expenses.reduce((total, item) => {
+     return (total += item.cost);
+   }, 0);
+
+
+   const remainingBudget = updatedBudget - totalExpenses;
+   dispatch({
+     type: 'SET_REMAINING',
+     payload: remainingBudget,
+   });
+
+
+   if (updatedBudget > 20000) {
+     alert('Budget cannot exceed 20,000!');
+   }
+
+
+   if (updatedBudget < totalExpenses) {
+     alert('Expenses value exceed the budget!');
+   }
+ }, [updatedBudget, expenses, dispatch]);
+
+
+ const handleBudgetChange = (event) => {
+   const value = parseInt(event.target.value);
+   if (!isNaN(value)) {
+     setUpdatedBudget(value);
+   }
+ };
+
+
+ return (
+   <div className='alert alert-secondary'>
+     <div className='input-group'>
+       <div className='input-group-prepend'>
+         <span className='input-group-text'>Budget:</span>
+       </div>
+       <input
+         type='number'
+         className='form-control'
+         value={updatedBudget}
+         onChange={handleBudgetChange}
+         step={10}
+         max={20000}
+       />
+     </div>
+   </div>
+ );
 };
+
+
 export default Budget;
